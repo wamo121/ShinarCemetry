18966713609a27316b6f823a2241c493ca6933d6
wamo121 <46901501+wamo121@users.noreply.github.com>
2021-05-01 21:42:24 -0700

    Create plot.sql

---
+++ plot.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 02, 2021 at 06:40 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shinarcemetry`
+--
+
+--
+-- Dumping data for table `plot`
+--
+
+INSERT INTO `plot` (`PlotID`, `PlotNum`, `Description`) VALUES
+(1, 1, 'Description of 1'),
+(2, 2, 'Description of plot 2'),
+(3, 3, 'Description of plot 3'),
+(4, 4, 'Description of plot 4');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
